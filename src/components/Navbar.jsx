bd8b582fa84122467e908162a750ae6cf37a4d65
Samir Hashimov <samirhasimov10@gmail.com>
2026-09-20 16:33:43 +0400

    feat: update Navbar component to use anchor tag for branding link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,10 +15,10 @@ const Navbar = ({ toggleTheme, isDarkMode }) => {
     return (
         <div className='sticky top-0 left-0 w-full z-[100] bg-bg transition-colors duration-300'>
             <div className='max-w-[1104px] mx-auto py-3 px-5 xl:px-0 flex justify-between items-center border-b-[1.2px] border-border bg-transparent'>
-                <div className='flex items-center gap-[10px]'>
+                <a href='/' className='flex items-center gap-[10px] no-underline'>
                     <img src={pp} className='w-[35px] rounded-full'></img>
                     <h1 className='font-[Inter] text-[1.2rem] text-text max-md:text-[1.1rem] font-bold'>Samir Hashimov</h1>
-                </div>
+                </a>
                 <div className='flex gap-3 items-center'>
                     <div className="flex gap-3 max-[480px]:hidden">
                         <a className='no-underline text-text font-[Inter] text-[0.8rem] border-b border-text py-[3px] transition-colors duration-200 hover:border-section-header hover:text-section-header max-[480px]:text-[0.75rem]' href='/'>{t('navbar.home')}</a>
